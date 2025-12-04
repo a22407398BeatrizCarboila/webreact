d8647d0e365cf03759a09f6a20638d069ab524c6
a22407398BeatrizCarboila <a22407398@alunos.ulht.pt>
2025-12-04 10:00:16 +0000

    updates

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import MagiaDoJSX from "@/components/MagiaDoJSX/MagiaDoJSX"
+export default function page(){
+  return(
+    <div>
+    <h2>Interfaces Modernos</h2>
+    <p>Bem vindo á minha app em React e em Next.js.</p>
+    <MagiaDoJSX />
+    </div>
+  )
+}
